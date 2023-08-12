3e2cd073b4e904711651fa92d3962ea9f5dff7f4
[Yirchew Daniel] <yirchewdanbonba@gmail.com>
2023-08-13 00:16:09 +0100

    c helloworld

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+int main(void)
+{
+
+char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, quote, sizeof(quote) - 1);
+return (1);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+printf("size of char:%lu\n", sizeof(char));
+printf(" size of int:%lu\n", sizeof(int));
+printf("size of float:%lu\n", sizeof(float));
+printf("size of double:%lu\n", sizeof(double));
+printf("size of long int:%lu\n", sizeof(long int));
+printf("size of long long int:%lu\n", sizeof(long long int));
+
+return (0);
+}
