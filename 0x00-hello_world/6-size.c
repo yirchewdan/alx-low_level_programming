ecad4008c5ab76d5a2aded03b116860d1eac89f4
[Yirchew Daniel] <yirchewdanbonba@gmail.com>
2023-08-13 00:32:12 +0100

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(void)
 {
-printf("size of char:%lu byte(s) long\n", sizeof(char));
-printf(" size of int:%lu byte(s) long\n", sizeof(int));
-printf("size of float:%lu byte(s) long\n", sizeof(float));
-printf("size of double:%lu byte(s) long\n", sizeof(double));
-printf("size of long int:%lu byte(s) long\n", sizeof(long int));
-printf("size of long long int:%lu byte(s) long\n", sizeof(long long int));
+printf("size of char:%lu byte(s)\n", sizeof(char));
+printf(" size of int:%lu byte(s)\n", sizeof(int));
+printf("size of float:%lu byte(s)\n", sizeof(float));
+printf("size of double:%lu byte(s)\n", sizeof(double));
+printf("size of long int:%lu byte(s)\n", sizeof(long int));
+printf("size of long long int:%lu byte(s)\n", sizeof(long long int));
 
 return (0);
 }
